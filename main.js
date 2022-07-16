bad2a8786061469be1d798954b313ec6833fe686
unknown <xogk1128@naver.com>
2022-07-16 16:37:59 +0900

    데이터베이스 와 연결

--- main.js
+++ main.js
@@ -21,17 +21,6 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        // fs.readdir('./data', function(error, filelist){
-        //   var title = 'Welcome';
-        //   var description = 'Hello, Node.js';
-        //   var list = template.list(filelist);
-        //   var html = template.HTML(title, list,
-        //     `<h2>${title}</h2>${description}`,
-        //     `<a href="/create">create</a>`
-        //   );
-        //   response.writeHead(200);
-        //   response.end(html);
-        // });
         db.query('SELECT * FROM topic',function(err,topics){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
@@ -69,11 +58,13 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
+
+      db.query('SELECT * FROM topic', function(err, topics){
         var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
+        
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -81,8 +72,10 @@ var app = http.createServer(function(request,response){
             <p>
               <input type="submit">
             </p>
-          </form>
-        `, '');
+          </form>`,
+          ''
+        );
+        console.log(topics);
         response.writeHead(200);
         response.end(html);
       });
@@ -93,55 +86,60 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
+          db.query(`INSERT INTO topic(title, description, created, author_id) VALUES(?, ?, NOW(), ?)`
+          , [post.title, post.description, 1]
+          , function(error, result){
+            if(error)throw error;
+            response.writeHead(302, {Location: `/?id=${result.insertId}`});
             response.end();
-          })
       });
+      });
+      
+
     } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      
+      db.query('SELECT * FROM topic',function(err,topics){
+        if(err)throw err;
+        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(err2,topic){
+          if(err2)throw err2;
+          var list = template.list(topics);
+          var html = template.HTML(topic[0], list,
+            `<form action="/update_process" method="post">
+            <input type="hidden" name="id" value="${topic[0].id}">
+            <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+            <p>
+              <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+            </form>`,
+            `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
           );
           response.writeHead(200);
           response.end(html);
         });
       });
+    
     } else if(pathname === '/update_process'){
+
       var body = '';
       request.on('data', function(data){
           body = body + data;
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
+        
+          db.query(`UPDATE topic SET title=?,description=?, author_id=1 WHERE id=?`,
+          [post.title, post.description, post.id],
+          function(err, result){
+            if(err)
+              throw err;
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
           });
       });
+
     } else if(pathname === '/delete_process'){
       var body = '';
       request.on('data', function(data){
@@ -149,13 +147,12 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
+          db.query(`DELETE FROM topic WHERE id=?`,[post.id],function(err, result){
             response.writeHead(302, {Location: `/`});
             response.end();
-          })
+          });
       });
+
     } else {
       response.writeHead(404);
       response.end('Not found');
